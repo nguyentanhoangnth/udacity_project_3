0898ca66d3e8ca27c340b45812e3b433cf6ee9bd
Hoang Nguyen <nguyentanhoang.nth@gmail.com>
2022-04-25 21:27:50 +0700

    Update code

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udadatabase
+export POSTGRES_PASSWORD=udadatabase
+export POSTGRES_HOST=udadatabase.cpllar3skhww.ap-southeast-1.rds.amazonaws.com
+export POSTGRES_DB=uda
+export AWS_BUCKET=uda-hoangnt33-dev
+export AWS_REGION=ap-southeast-1
+export AWS_PROFILE=admin
+export JWT_SECRET=hello
+export URL=http://localhost:8100
